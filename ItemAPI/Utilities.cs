3d9e688bd7b49ec6444bdcfd8b543e5f018fa55f
help <39848574+blazeykat@users.noreply.github.com>
2020-09-22 17:28:34 +1000

    Added new methods

--- ItemAPI/Utilities.cs
+++ ItemAPI/Utilities.cs
@@ -1,4 +1,4 @@
-﻿using katmod;
+using katmod;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -133,19 +133,6 @@ namespace ItemAPI
 			}
 		}
 
-		public static void Kill(this HealthHaver health)
-        {
-			health.ApplyDamage(1E+07f, Vector2.zero, "dying, lmao", CoreDamageTypes.None, DamageCategory.Normal, true, null, false);
-		}
-
-		/// <summary>
-		/// unstoppable and imminent death lol
-		/// </summary>
-		public static void Obliterate(this HealthHaver health)
-		{
-			health.ApplyDamage(1E+07f, Vector2.zero, "dying, lmao", CoreDamageTypes.None, DamageCategory.Unstoppable, true, null, true);
-		}
-
 		public static Projectile HandleProjectile(this PlayerController player, float speed, float damage, int ProjectileID)
         {
 			Projectile projectile2 = ((Gun)ETGMod.Databases.Items[ProjectileID]).DefaultModule.projectiles[0];
@@ -158,6 +145,7 @@ namespace ItemAPI
 				component.Shooter = player.specRigidbody;
 				component.baseData.speed = speed;
 				component.baseData.damage = damage;
+				player.DoPostProcessProjectile(component);
 			}
 			return component;
 		}
@@ -174,6 +162,7 @@ namespace ItemAPI
 				component.Shooter = player.specRigidbody;
 				component.baseData.speed = speed;
 				component.baseData.damage = damage;
+				player.DoPostProcessProjectile(component);
 			}
 			return component;
 		}
@@ -191,6 +180,41 @@ namespace ItemAPI
 				component.baseData.speed = speed;
 				component.baseData.damage = damage;
 				component.baseData.range = range;
+				player.DoPostProcessProjectile(component);
+			}
+			return component;
+		}
+
+		public static Projectile HandleProjectileAimedFromDifferentPosition(this SpeculativeRigidbody position, PlayerController player, float speed, float damage, int ProjectileID, float aim, float range)
+		{
+			Projectile projectile2 = ((Gun)ETGMod.Databases.Items[ProjectileID]).DefaultModule.projectiles[0];
+			GameObject gameObject = SpawnManager.SpawnProjectile(projectile2.gameObject, position.UnitCenter, Quaternion.Euler(0f, 0f, aim), true);
+			Projectile component = gameObject.GetComponent<Projectile>();
+			bool componentless = component != null;
+			if (componentless)
+			{
+				component.Owner = player;
+				component.Shooter = player.specRigidbody;
+				component.baseData.speed = speed;
+				component.baseData.damage = damage;
+				component.baseData.range = range;
+				player.DoPostProcessProjectile(component);
+			}
+			return component;
+		}
+		public static Projectile HandleProjectileAimedFromDifferentPosition(this tk2dBaseSprite position, PlayerController player, float speed, float damage, int ProjectileID, float aim)
+		{
+			Projectile projectile2 = ((Gun)ETGMod.Databases.Items[ProjectileID]).DefaultModule.projectiles[0];
+			GameObject gameObject = SpawnManager.SpawnProjectile(projectile2.gameObject, position.WorldCenter, Quaternion.Euler(0f, 0f, aim), true);
+			Projectile component = gameObject.GetComponent<Projectile>();
+			bool componentless = component != null;
+			if (componentless)
+			{
+				component.Owner = player;
+				component.Shooter = player.specRigidbody;
+				component.baseData.speed = speed;
+				component.baseData.damage = damage;
+				player.DoPostProcessProjectile(component);
 			}
 			return component;
 		}
@@ -208,6 +232,7 @@ namespace ItemAPI
 				component.baseData.speed = speed;
 				component.baseData.damage = damage;
 				component.baseData.range = range;
+				player.DoPostProcessProjectile(component);
 			}
 			return component;
 		}
@@ -244,47 +269,196 @@ namespace ItemAPI
 		}
 
 		/// <summary>
-		/// Ask neighborino when this goes wrong
+		/// Ask spapi when this goes wrong
 		/// </summary>
-		public static void SetProjectileSpriteRight(this Projectile proj, string name, int pixelWidth, int pixelHeight, int? overrideColliderPixelWidth = null, int? overrideColliderPixelHeight = null)
+		public static void ConstructOffsetsFromAnchor(this tk2dSpriteDefinition def, tk2dBaseSprite.Anchor anchor, Vector2? scale = null, bool fixesScale = false, bool changesCollider = true)
+		{
+			if (!scale.HasValue)
+			{
+				scale = new Vector2?(def.position3);
+			}
+			if (fixesScale)
+			{
+				Vector2 fixedScale = scale.Value - def.position0.XY();
+				scale = new Vector2?(fixedScale);
+			}
+			float xOffset = 0;
+			if (anchor == tk2dBaseSprite.Anchor.LowerCenter || anchor == tk2dBaseSprite.Anchor.MiddleCenter || anchor == tk2dBaseSprite.Anchor.UpperCenter)
+			{
+				xOffset = -(scale.Value.x / 2f);
+			}
+			else if (anchor == tk2dBaseSprite.Anchor.LowerRight || anchor == tk2dBaseSprite.Anchor.MiddleRight || anchor == tk2dBaseSprite.Anchor.UpperRight)
+			{
+				xOffset = -scale.Value.x;
+			}
+			float yOffset = 0;
+			if (anchor == tk2dBaseSprite.Anchor.MiddleLeft || anchor == tk2dBaseSprite.Anchor.MiddleCenter || anchor == tk2dBaseSprite.Anchor.MiddleLeft)
+			{
+				yOffset = -(scale.Value.y / 2f);
+			}
+			else if (anchor == tk2dBaseSprite.Anchor.UpperLeft || anchor == tk2dBaseSprite.Anchor.UpperCenter || anchor == tk2dBaseSprite.Anchor.UpperRight)
+			{
+				yOffset = -scale.Value.y;
+			}
+			def.MakeOffset(new Vector2(xOffset, yOffset), changesCollider);
+		}
+
+		public static void MakeOffset(this tk2dSpriteDefinition def, Vector2 offset, bool changesCollider = false)
+		{
+			float xOffset = offset.x;
+			float yOffset = offset.y;
+			def.position0 += new Vector3(xOffset, yOffset, 0);
+			def.position1 += new Vector3(xOffset, yOffset, 0);
+			def.position2 += new Vector3(xOffset, yOffset, 0);
+			def.position3 += new Vector3(xOffset, yOffset, 0);
+			def.boundsDataCenter += new Vector3(xOffset, yOffset, 0);
+			def.boundsDataExtents += new Vector3(xOffset, yOffset, 0);
+			def.untrimmedBoundsDataCenter += new Vector3(xOffset, yOffset, 0);
+			def.untrimmedBoundsDataExtents += new Vector3(xOffset, yOffset, 0);
+			if (def.colliderVertices != null && def.colliderVertices.Length > 0 && changesCollider)
+			{
+				def.colliderVertices[0] += new Vector3(xOffset, yOffset, 0);
+			}
+		}
+
+		public static void SetProjectileSpriteRight(this Projectile proj, string name, int pixelWidth, int pixelHeight, bool lightened = true, tk2dBaseSprite.Anchor anchor = tk2dBaseSprite.Anchor.LowerLeft, bool anchorChangesCollider = true,
+				bool fixesScale = false, int? overrideColliderPixelWidth = null, int? overrideColliderPixelHeight = null, int? overrideColliderOffsetX = null, int? overrideColliderOffsetY = null, Projectile overrideProjectileToCopyFrom = null)
 		{
 			try
 			{
-				bool flag = overrideColliderPixelWidth == null;
-				if (flag)
-				{
-					overrideColliderPixelWidth = new int?(pixelWidth);
-				}
-				bool flag2 = overrideColliderPixelHeight == null;
-				if (flag2)
-				{
-					overrideColliderPixelHeight = new int?(pixelHeight);
-				}
-				float num = (float)pixelWidth / 16f;
-				float num2 = (float)pixelHeight / 16f;
-				float x = (float)overrideColliderPixelWidth.Value / 16f;
-				float y = (float)overrideColliderPixelHeight.Value / 16f;
 				proj.GetAnySprite().spriteId = ETGMod.Databases.Items.ProjectileCollection.inst.GetSpriteIdByName(name);
-				tk2dSpriteDefinition tk2dSpriteDefinition = ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[(PickupObjectDatabase.GetById(12) as Gun).DefaultModule.projectiles[0].GetAnySprite().spriteId].CopyDefinitionFrom();
-				tk2dSpriteDefinition.boundsDataCenter = new Vector3(num / 2f, num2 / 2f, 0f);
-				tk2dSpriteDefinition.boundsDataExtents = new Vector3(num, num2, 0f);
-				tk2dSpriteDefinition.untrimmedBoundsDataCenter = new Vector3(num / 2f, num2 / 2f, 0f);
-				tk2dSpriteDefinition.untrimmedBoundsDataExtents = new Vector3(num, num2, 0f);
-				tk2dSpriteDefinition.position0 = new Vector3(0f, 0f, 0f);
-				tk2dSpriteDefinition.position1 = new Vector3(0f + num, 0f, 0f);
-				tk2dSpriteDefinition.position2 = new Vector3(0f, 0f + num2, 0f);
-				tk2dSpriteDefinition.position3 = new Vector3(0f + num, 0f + num2, 0f);
-				tk2dSpriteDefinition.colliderVertices[1].x = x;
-				tk2dSpriteDefinition.colliderVertices[1].y = y;
-				tk2dSpriteDefinition.name = name;
-				ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[proj.GetAnySprite().spriteId] = tk2dSpriteDefinition;
-				proj.baseData.force = 0f;
+				tk2dSpriteDefinition def = SetupDefinitionForProjectileSprite(name, proj.GetAnySprite().spriteId, pixelWidth, pixelHeight, lightened, overrideColliderPixelWidth, overrideColliderPixelHeight, overrideColliderOffsetX,
+					overrideColliderOffsetY, overrideProjectileToCopyFrom);
+				def.ConstructOffsetsFromAnchor(anchor, def.position3, fixesScale, anchorChangesCollider);
+				proj.GetAnySprite().scale = new Vector3(1f, 1f, 1f);
+				proj.transform.localScale = new Vector3(1f, 1f, 1f);
+				proj.GetAnySprite().transform.localScale = new Vector3(1f, 1f, 1f);
+				proj.AdditionalScaleMultiplier = 1f;
 			}
 			catch (Exception ex)
 			{
-				ETGModConsole.Log("Ooops! Seems like something got very, Very, VERY wrong. Here's the exception:", false);
-				ETGModConsole.Log(ex.ToString(), false);
+				ETGModConsole.Log("Ooops! Seems like something got very, Very, VERY wrong. Here's the exception:");
+				ETGModConsole.Log(ex.ToString());
 			}
+		}
+
+		public static tk2dSpriteDefinition SetupDefinitionForProjectileSprite(string name, int id, int pixelWidth, int pixelHeight, bool lightened = true, int? overrideColliderPixelWidth = null, int? overrideColliderPixelHeight = null,
+			int? overrideColliderOffsetX = null, int? overrideColliderOffsetY = null, Projectile overrideProjectileToCopyFrom = null)
+		{
+			if (overrideColliderPixelWidth == null)
+			{
+				overrideColliderPixelWidth = pixelWidth;
+			}
+			if (overrideColliderPixelHeight == null)
+			{
+				overrideColliderPixelHeight = pixelHeight;
+			}
+			if (overrideColliderOffsetX == null)
+			{
+				overrideColliderOffsetX = 0;
+			}
+			if (overrideColliderOffsetY == null)
+			{
+				overrideColliderOffsetY = 0;
+			}
+			float thing = 14;
+			float thing2 = 16;
+			float trueWidth = (float)pixelWidth / thing;
+			float trueHeight = (float)pixelHeight / thing;
+			float colliderWidth = (float)overrideColliderPixelWidth.Value / thing2;
+			float colliderHeight = (float)overrideColliderPixelHeight.Value / thing2;
+			float colliderOffsetX = (float)overrideColliderOffsetX.Value / thing2;
+			float colliderOffsetY = (float)overrideColliderOffsetY.Value / thing2;
+			tk2dSpriteDefinition def = ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[(overrideProjectileToCopyFrom ??
+					(PickupObjectDatabase.GetById(lightened ? 47 : 12) as Gun).DefaultModule.projectiles[0]).GetAnySprite().spriteId].CopyDefinitionFrom();
+			def.boundsDataCenter = new Vector3(trueWidth / 2f, trueHeight / 2f, 0f);
+			def.boundsDataExtents = new Vector3(trueWidth, trueHeight, 0f);
+			def.untrimmedBoundsDataCenter = new Vector3(trueWidth / 2f, trueHeight / 2f, 0f);
+			def.untrimmedBoundsDataExtents = new Vector3(trueWidth, trueHeight, 0f);
+			def.texelSize = new Vector2(1 / 16f, 1 / 16f);
+			def.position0 = new Vector3(0f, 0f, 0f);
+			def.position1 = new Vector3(0f + trueWidth, 0f, 0f);
+			def.position2 = new Vector3(0f, 0f + trueHeight, 0f);
+			def.position3 = new Vector3(0f + trueWidth, 0f + trueHeight, 0f);
+			def.colliderVertices[0].x = colliderOffsetX;
+			def.colliderVertices[0].y = colliderOffsetY;
+			def.colliderVertices[1].x = colliderWidth;
+			def.colliderVertices[1].y = colliderHeight;
+			def.name = name;
+			ETGMod.Databases.Items.ProjectileCollection.inst.spriteDefinitions[id] = def;
+			return def;
+		}
+
+		public static void AnimateProjectile(this Projectile proj, List<string> names, int fps, bool loops, List<IntVector2> pixelSizes, List<bool> lighteneds, List<tk2dBaseSprite.Anchor> anchors, List<bool> anchorsChangeColliders,
+			List<bool> fixesScales, List<Vector3?> manualOffsets, List<IntVector2?> overrideColliderPixelSizes, List<IntVector2?> overrideColliderOffsets, List<Projectile> overrideProjectilesToCopyFrom)
+		{
+			tk2dSpriteAnimationClip clip = new tk2dSpriteAnimationClip();
+			clip.name = "idle";
+			clip.fps = fps;
+			List<tk2dSpriteAnimationFrame> frames = new List<tk2dSpriteAnimationFrame>();
+			for (int i = 0; i < names.Count; i++)
+			{
+				string name = names[i];
+				IntVector2 pixelSize = pixelSizes[i];
+				IntVector2? overrideColliderPixelSize = overrideColliderPixelSizes[i];
+				IntVector2? overrideColliderOffset = overrideColliderOffsets[i];
+				Vector3? manualOffset = manualOffsets[i];
+				bool anchorChangesCollider = anchorsChangeColliders[i];
+				bool fixesScale = fixesScales[i];
+				if (!manualOffset.HasValue)
+				{
+					manualOffset = new Vector2?(Vector2.zero);
+				}
+				tk2dBaseSprite.Anchor anchor = anchors[i];
+				bool lightened = lighteneds[i];
+				Projectile overrideProjectileToCopyFrom = overrideProjectilesToCopyFrom[i];
+				tk2dSpriteAnimationFrame frame = new tk2dSpriteAnimationFrame();
+				frame.spriteId = ETGMod.Databases.Items.ProjectileCollection.inst.GetSpriteIdByName(name);
+				frame.spriteCollection = ETGMod.Databases.Items.ProjectileCollection;
+				frames.Add(frame);
+				int? overrideColliderPixelWidth = null;
+				int? overrideColliderPixelHeight = null;
+				if (overrideColliderPixelSize.HasValue)
+				{
+					overrideColliderPixelWidth = overrideColliderPixelSize.Value.x;
+					overrideColliderPixelHeight = overrideColliderPixelSize.Value.y;
+				}
+				int? overrideColliderOffsetX = null;
+				int? overrideColliderOffsetY = null;
+				if (overrideColliderOffset.HasValue)
+				{
+					overrideColliderOffsetX = overrideColliderOffset.Value.x;
+					overrideColliderOffsetY = overrideColliderOffset.Value.y;
+				}
+				tk2dSpriteDefinition def = SetupDefinitionForProjectileSprite(name, frame.spriteId, pixelSize.x, pixelSize.y, lightened, overrideColliderPixelWidth, overrideColliderPixelHeight, overrideColliderOffsetX, overrideColliderOffsetY,
+					overrideProjectileToCopyFrom);
+				def.ConstructOffsetsFromAnchor(anchor, def.position3, fixesScale, anchorChangesCollider);
+				def.position0 += manualOffset.Value;
+				def.position1 += manualOffset.Value;
+				def.position2 += manualOffset.Value;
+				def.position3 += manualOffset.Value;
+				if (i == 0)
+				{
+					proj.GetAnySprite().SetSprite(frame.spriteCollection, frame.spriteId);
+				}
+			}
+			clip.wrapMode = loops ? tk2dSpriteAnimationClip.WrapMode.Loop : tk2dSpriteAnimationClip.WrapMode.Once;
+			clip.frames = frames.ToArray();
+			if (proj.sprite.spriteAnimator == null)
+			{
+				proj.sprite.spriteAnimator = proj.sprite.gameObject.AddComponent<tk2dSpriteAnimator>();
+			}
+			proj.sprite.spriteAnimator.playAutomatically = true;
+			bool flag = proj.sprite.spriteAnimator.Library == null;
+			if (flag)
+			{
+				proj.sprite.spriteAnimator.Library = proj.sprite.spriteAnimator.gameObject.AddComponent<tk2dSpriteAnimation>();
+				proj.sprite.spriteAnimator.Library.clips = new tk2dSpriteAnimationClip[0];
+				proj.sprite.spriteAnimator.Library.enabled = true;
+			}
+			proj.sprite.spriteAnimator.Library.clips = proj.sprite.spriteAnimator.Library.clips.Concat(new tk2dSpriteAnimationClip[] { clip }).ToArray();
+			proj.sprite.spriteAnimator.DefaultClipId = proj.sprite.spriteAnimator.Library.GetClipIdByName("idle");
+			proj.sprite.spriteAnimator.deferNextStartClip = false;
 		}
 
 		public static tk2dSpriteDefinition CopyDefinitionFrom(this tk2dSpriteDefinition other)
@@ -419,7 +593,7 @@ namespace ItemAPI
 				Projectile component = player.HandleChargeProjectile(20f, 7f, 620);
 				component.AddHoming();
 			}
-			if (player.HasPassiveItem(CatSnack.ID))
+			if (player.HasMTGConsoleID("psm:cat_snack"))
 			{
 				if (Utilities.UncoolRandom(0.97f))
 				{
@@ -459,6 +633,18 @@ namespace ItemAPI
 				return PickupObject.ItemQuality.A;
 			}
 			return PickupObject.ItemQuality.S;
+		}
+
+		public static void Log(this string stringy)
+        {
+			ETGModConsole.Log(stringy);
+        }
+
+		public static void Notify(string header, string text, string spriteID, UINotificationController.NotificationColor color = UINotificationController.NotificationColor.SILVER)
+		{
+			tk2dSpriteCollectionData encounterIconCollection = AmmonomiconController.Instance.EncounterIconCollection;
+			int spriteIdByName = encounterIconCollection.GetSpriteIdByName(spriteID);
+			GameUIRoot.Instance.notificationController.DoCustomNotification(header, text, encounterIconCollection, spriteIdByName, color, false, false);
 		}
 	}
 }
