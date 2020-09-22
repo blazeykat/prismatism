deecf1d7ea2ba3b41218a0a0c3abf55028aef441
help <39848574+blazeykat@users.noreply.github.com>
2020-09-22 17:31:20 +1000

    module.cs is cooler now

--- Scripts/Module.cs
+++ Scripts/Module.cs
@@ -1,6 +1,5 @@
-﻿using System;
+using System;
 using UnityEngine;
-using katmod.ExpandAudio;
 using ItemAPI;
 using Dungeonator;
 using System.IO;
@@ -12,14 +11,13 @@ namespace katmod
     public class Module : ETGModule
     {
         public static readonly string MOD_NAME = "Prismatism";
-        public static readonly string VERSION = "0.1.9";
+        public static readonly string VERSION = "0.1.96 (The Reworkening)";
         public static readonly string TEXT_COLOR = "#7289da";
 
         public override void Init()
         {
             try
             {
-                AudioResourceLoader.InitAudio();
             } catch (Exception E)
             {
                 ETGModConsole.Log($"{E}");
@@ -42,6 +40,7 @@ namespace katmod
                 Jeremy.Init();
                 Gasoline.Init();
                 CatSnack.Init();
+                ClassicCatSnack.Init();
                 JunkSynthesizer.Init();
 
                 WyrmBlood.Init();
@@ -50,19 +49,17 @@ namespace katmod
                 KeyCandy.Init();
                 GoldenCandy.Init();
                 BlueCandy.Init();
-                MonsterCandy.Init();
-                HeartCandy.Init();
                 RedCandy.Init();
+                HeartCandy.Init();
+                MonsterCandy.Init();
                 TrickOTreator.Init();
 
                 BlackSkull.Init();
-                Randy.Init();
                 StoneSword.Init();
                 StoneAmmolet.Init();
 
                 BeeGun.Add();
-                //Ak_01.Add(); 
-
+                
                 PetrifyingMirror.Init();
                 EnchantedTome.Init();
                 YellowKey.Init();
@@ -71,10 +68,10 @@ namespace katmod
                 BloodBullets.Init();
                 HoodedShells.Init();
                 ClockworkCog.Init();
-                HyperBullets.Init();
+                HighPriestCloak.Init();
                 LuckyCoin.Init();
                 LuckyHorseshoe.Init();
-                BoilingFungus.Init();
+                ToxicFungus.Init();
                 PurpleGuonStone.Init();
                 GarbageBin.Init();
                 ColdAmmo.Init();
@@ -89,35 +86,17 @@ namespace katmod
                 ElectricRounds.Init();
                 ThunderRounds.Init();
                 ToxicHoneycomb.Init();
+
+                LandOfTheSynergies.AddSynergies();
             }
             catch (Exception ex)
             {
                 itemsLoad = $"{ex}";
-            }
-            /*try
-            {
-
-                GungeonInit.Init();
-                GooptonShrine.Add();
-                LowPriestShrine.Add();
-                GooptonIcon.Init();
-                LowPriestIcon.Init();
-            } catch (Exception E)
-            {
-                shrinesLoad = $"{E}";
-            }*/
-            /*try
-            {
-                BlueBubbler.Init();
-            } catch (Exception e)
-            {
-                enemiesLoad = $"{e}";
-            }*/
+            }               
             Log($"{MOD_NAME} v{VERSION} has started.", TEXT_COLOR);
             Log("Checklist:", TEXT_COLOR);
             Log($"Items: {itemsLoad}", TEXT_COLOR);
             Log("bonbo's world", TEXT_COLOR);
-            //Log($"Enemies: {enemiesLoad}", TEXT_COLOR);
         }
 
 
